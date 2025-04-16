1a986dfa23d9e87b0fa2c6052cec5e9789602fdb
Vhutuy <130938285+Vhutuy@users.noreply.github.com>
2025-04-16 19:16:40 -0300

    Update CorsConfig to render deploy

--- src/main/java/com/example/login_auth_api/infra/cors/CorsConfig.java
+++ src/main/java/com/example/login_auth_api/infra/cors/CorsConfig.java
@@ -10,7 +10,7 @@ public class CorsConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://localhost:4200", "https://projeto-patas.onrender.com/home")
+                .allowedOrigins("http://localhost:4200", "https://projeto-patas.onrender.com")
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                 .allowedHeaders("*")
                 .allowCredentials(true);
