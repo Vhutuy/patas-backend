c80b8d2c38108424e042b339701ca374aa442e67
Vhutuy <130938285+Vhutuy@users.noreply.github.com>
2025-06-24 23:20:33 -0300

    Quick Fix on animalController

--- src/main/java/com/example/login_auth_api/repositories/AnimalRepository.java
+++ src/main/java/com/example/login_auth_api/repositories/AnimalRepository.java
@@ -3,6 +3,8 @@ package com.example.login_auth_api.repositories;
 import com.example.login_auth_api.domain.animal.Animal;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface AnimalRepository extends JpaRepository<Animal, Long> {
+import java.util.UUID;
+
+public interface AnimalRepository extends JpaRepository<Animal, UUID> {
 
 }
